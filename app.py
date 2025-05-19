8d48dae114671b79111b331beec3dffaed22c824
miguel110517 <miguelmaceita@gmail.com>
2025-05-19 16:27:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -123,4 +123,7 @@ def predict_batch():
                            modelo_seleccionado=modelo_nombre)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
